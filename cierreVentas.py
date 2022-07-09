5d175a6095ee9e6fb6457984b8bb8fc3a4cd8787
jtibau <josetibau@gmail.com>
2022-07-09 17:18:54 -0500

    Fin Frames Fijos

--- cierreVentas.py
+++ cierreVentas.py
@@ -40,9 +40,9 @@ def crearWidgetsHeader(headerFrame, root):
     fechaLabel = tk.Label(identFrame, text=ahora.strftime('%d-%m-%Y'), bg=defaultColor)
     userLabel = tk.Label(identFrame, text=usuario['nombre']+' '+usuario['apellido'], bg=defaultColor)
 
-    localLabel.pack(fill='both')
-    fechaLabel.pack(fill='both')
-    userLabel.pack(fill='both')
+    localLabel.pack(fill='both', padx=5)
+    fechaLabel.pack(fill='both', padx=5)
+    userLabel.pack(fill='both', padx=5)
 
     logoLabel.pack(side='left', fill='both')
     identFrame.pack(side='right')
@@ -76,8 +76,8 @@ def crearFrames(root):
     commandFrame.pack(side='left', fill='y')
     crearWidgetsCommand(commandFrame, root)
 
-    dataFrame  = tk.Frame(root, bg=defaultColor)
-    dataFrame.pack(side='left', fill='y')
+    # dataFrame  = tk.Frame(root, bg=defaultColor)
+    # dataFrame.pack(side='left', fill='y')
 
 def cerrarVentana():
     pass
@@ -99,6 +99,7 @@ def main():
     rootStyle = ttk.Style(root)
 
     rootStyle.configure('TLabel', bg=defaultColor)
+    rootStyle.configure('TFrame', bg=defaultColor)
    
     cargarDatosGlobales()
     crearFrames(root)
@@ -108,73 +109,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-# class IdentFrame(tk.Frame):
-#     def __init__(self, container):
-#         super().__init__(self)
-
-#         self.__createWidgets()
-
-#     def __createWidgets(self):
-#         localLabel = tk.Label(text='Nombre del Local')
-#         fechaLabel = tk.Label(text='Fecha y hora')
-#         userLabel = tk.Label(text='Nombre del usuario')
-
-#         localLabel.pack(ipadx=5, ipady=5, fill='x')
-#         fechaLabel.pack(ipadx=5, ipady=5, fill='x')
-#         userLabel.pack(ipadx=5, ipady=5, fill='x')
-
-# class HeaderFrame(tk.Frame):
-#     def __init__(self, app):
-#         super().__init__()
-
-#         self.config(fg='red')
-
-#         self.__createWidgets()
-
-#     def __createWidgets(self):
-#         logoLabel = tk.Label()
-#         tituloLabel = tk.Label(text='Cierre Diario de Ventas')
-#         idFrame = IdentFrame(self)
-
-#         logoLabel.pack(side='left')
-#         tituloLabel.pack(side='left')
-#         idFrame.pack(side='right')
-        
-
-# class CommandFrame(tk.Frame):
-#     def __init__(self, app):
-#         pass
-
-# class BottomFrame(tk.Frame):
-#     def __init__(self, app):
-#         pass
-
-# class DataFrames(tk.Frame):
-#     def __init__(self, app):
-#         pass
-
-
-# class App(tk.Tk):
-#     def __init__(self):
-#         super().__init__()
-
-#         defaultColor = '#FFF8EF'
-
-#         self.title('')
-#         self.geometry('800x600')
-#         self.config(bg=defaultColor)  
-        
-#     def __createFrames(self):
-#         headerFrame = HeaderFrame(self)
-#         commandFrame = CommandFrame(self)
-#         dataFrames = DataFrames(self)
-
-#         headerFrame.pack(self, fill='x')
-
-
-# if __name__ == '__main__':
-#     app = App()
-#     app.mainloop()
